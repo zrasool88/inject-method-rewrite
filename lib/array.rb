83be2579f0a81e8b9bce47f88d8935391128a5ad
Zeeshan Rasool <zrasool88@gmail.com>
2015-05-02 00:59:19 +0100

    refactoring the code. breaking the pinject method into smaller methods

--- lib/array.rb
+++ lib/array.rb
@@ -1,9 +1,27 @@
 class Array
   def pinject(*args, &block)
+    if args[0].is_a? Symbol
+      pinject_with_symbol(*args, &block)
+    else
+      pinject_with_block(*args, &block)
+    end
+  end
+
+  def pinject_with_symbol(*args, &block)
     copy_of_array = self.dup
-    accumulator = copy_of_array.shift if (args[0].is_a? Symbol) || (args.empty?)
-    accumulator = args[0] if (!args[0].is_a? Symbol) && (!args.empty?)
-    block = args[0].to_proc if args[0].is_a? Symbol
+    accumulator = copy_of_array.shift
+    block = args[0].to_proc
+    copy_of_array.each { |value| accumulator = block.call(accumulator, value) }
+    accumulator
+  end
+
+  def pinject_with_block(*args, &block)
+    copy_of_array = self.dup
+    if args.empty?
+      accumulator = copy_of_array.shift
+    else
+      accumulator = args[0]
+    end
     copy_of_array.each { |value| accumulator = block.call(accumulator, value) }
     accumulator
   end
